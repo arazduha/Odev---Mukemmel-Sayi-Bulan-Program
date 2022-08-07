acc7a3b5578bed1651717e384a2618f6fb6c5c17
Muhammed Duha <107310860+arazduha@users.noreply.github.com>
2022-08-07 13:25:04 +0300

    Update test3.java

--- src/test3.java
+++ src/test3.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 public class test3 {
     public static  void main (String[] args) {
         Scanner input = new Scanner(System.in);
-        int a, toplam = 0;
         while (true) {
+            int a, toplam = 0;
             System.out.print("Bir sayi giriniz: ");
             a = input.nextInt();
             for (int i = 1; i < a; i++) {
